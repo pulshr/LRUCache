c0b138fea080bb6fd8746b8c296cd8266a9018ef
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-09-17 15:49:45 +0530

    Implemented DLL

--- src/org/pulkitshrivastava/Main.java
+++ src/org/pulkitshrivastava/Main.java
@@ -1,8 +1,32 @@
 package org.pulkitshrivastava;
 
-public class Main {
+import java.util.HashMap;
 
+public class Main {
     public static void main(String[] args) {
 	// write your code here
+    }
+    public static class LRUCache {
+        int capacity;
+        public static class DoublyLinkedList {
+            int key;
+            int value;
+            DoublyLinkedList previousNode;
+            DoublyLinkedList nextNode;
+            public DoublyLinkedList(int key, int value) {
+                this.key = key;
+                this.value = value;
+                this.previousNode = null;
+                this.nextNode = null;
+            }
+        }
+        HashMap<Integer, DoublyLinkedList> cache;
+        public DoublyLinkedList headNode = null;
+        public DoublyLinkedList tailNode = null;
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            cache = new HashMap<>(this.capacity);
+        }
+
     }
 }
