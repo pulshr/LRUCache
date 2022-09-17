a03720b2eb2d161113f27481ce7729a29b8142be
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-09-17 16:15:32 +0530

    Implemented removeNode and insertAtLast method

--- src/org/pulkitshrivastava/Main.java
+++ src/org/pulkitshrivastava/Main.java
@@ -27,6 +27,42 @@ public class Main {
             this.capacity = capacity;
             cache = new HashMap<>(this.capacity);
         }
+        public void removeNode(DoublyLinkedList currentNode)
+        {
+            if ( currentNode.nextNode == null && currentNode.previousNode == null )
+            {
+                headNode = null;
+                tailNode = null;
+                return;
+            }
 
+            if ( currentNode == tailNode )
+            {
+                tailNode = currentNode.previousNode;
+                return;
+            }
+
+            if ( headNode == currentNode )
+                headNode = headNode.nextNode;
+            DoublyLinkedList currentPreviousNode = currentNode.previousNode;
+            DoublyLinkedList currentNextNode = currentNode.nextNode;
+            currentNextNode.previousNode = currentPreviousNode;
+            if (currentPreviousNode != null)
+            {
+                currentPreviousNode.nextNode = currentNextNode;
+            }
+        }
+        public void insertAtLast(DoublyLinkedList currentNode)
+        {
+            if (headNode == null)
+            {
+                headNode = currentNode;
+                tailNode = currentNode;
+                return;
+            }
+            tailNode.nextNode = currentNode;
+            currentNode.previousNode = tailNode;
+            tailNode = currentNode;
+        }
     }
 }
